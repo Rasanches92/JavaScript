ef8d1f6d59a79ed8278b6883cf945c01f090af69
Rafael Chagas Sanches <rafa_0033@hotmail.com>
2022-08-21 22:56:42 -0300

    Add files via upload
    Aula realizada em 21.08.2022

---
+++ Tipos_Variaveis_Funcoes/[08.21]parametros.js
@@ -0,0 +1,26 @@
+//parâmetros de função servem para a a função receber informações para executar corretamente
+
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+//console.log(soma(2,2));
+//console.log(soma(245,20));
+//console.log(soma(-500,60));
+
+// parâmetros x argumentos
+
+// ordem dos parâmetros
+
+function nomeIdade (nome, idade){
+    return `meu nome é ${nome} e minha idade é ${idade}`;
+}
+
+//console.log(nomeIdade(30, "Rafael"))
+
+function multiplica(numero1 = 1, numero2 = 1){
+    return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4, 5)));
+
---
+++ Tipos_Variaveis_Funcoes/[08.21]template-strings.js
@@ -0,0 +1,10 @@
+const nome = "Rafa";
+const idade = 2022-1992;
+const cidadeDeNascimento = "São Paulo";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome} , minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+console.log(apresentacao);
+
+
